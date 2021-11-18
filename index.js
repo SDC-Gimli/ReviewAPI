8aec7a9ee90f10c20a93685f88d590b71288e550
mkr123 <mankairui@yahoo.com>
2021-11-18 12:18:25 -0800

    implement get reviews api endpoint

--- index.js
+++ index.js
@@ -4,9 +4,24 @@ const PORT = 3000;
 const app = express();
 const db = require('./database');
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.get("/reviews",(req,res)=>{
- console.log(req.query.product_id);
+app.use(express.urlencoded({ extended: true }));
+app.get("/reviews", (req, res) => {
+  console.log(req.query.product_id);
+  console.log(req.query);
+  let page = req.query.page || 1;
+  let count = req.query.count || 5;
+  let sort = ""
+  switch (req.query.sort) {
+    case "newest":
+      sort = "date";
+      break;
+    case "helpful":
+      sort = "helpfulness";
+      break;
+    default:
+      sort = "date";
+  }
+
   let query = `SELECT
    reviews.review_id,
    reviews.rating,
@@ -16,16 +31,27 @@ app.get("/reviews",(req,res)=>{
    reviews.recommend,
    reviews.reviewer_name,
    reviews.response,
-   reviews.helpfulness
-   FROM reviews
-   WHERE reviews.product_id=${req.query.product_id};`
-  db.query(query).then((data)=>{
-      console.log("sucess query db",data)
-      res
-    }).catch((err)=>{
-      console.log(err);
-    })
-  });
+   reviews.helpfulness,
+   json_agg(json_build_object('id', reviews_photos.id, 'url', reviews_photos.url)) AS photos
+   FROM reviews LEFT JOIN reviews_photos
+   ON reviews_photos.review_id = reviews.review_id
+   WHERE reviews.product_id=${req.query.product_id}
+   GROUP BY reviews.review_id
+   ORDER BY ${sort} desc
+   Limit ${count};`
+  db.query(query).then((data) => {
+    console.log("sucess query db", data)
+    res.send(data);
+  }).catch((err) => {
+    console.log(err);
+  })
+});
+app.post('/reviews', (req, res) =>{
+  console.log(req.body)
+})
+
+
+
 
 
 app.listen(PORT, () => {
